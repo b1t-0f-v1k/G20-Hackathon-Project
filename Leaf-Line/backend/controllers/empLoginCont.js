981ad08bf2cdbcf39d732fb9b4adff9521797c2c
b1t-0f-v1k <victorsamehumadu@gmail.com>
2025-07-28 16:00:05 +0200

    vic changes 3

--- Leaf-Line/backend/controllers/empLoginCont.js
+++ Leaf-Line/backend/controllers/empLoginCont.js
@@ -4,31 +4,30 @@ const express = require('express');
 const router = express.Router();
 
 // fetching the relevent file using file path
-const User = require('../models/employeeSignUp');
+const User = require('../models/employeeLogin');
 
 // Asynchronous function for requesting and responding to user inputs
-const registerEmployee = async (req, res) => {
+const loginEmployee = async (req, res) => {
 
     // try-catch for error handling
     try {
         
-        const {email, username, password, businessName, businessID} = req.body;
+        const {email, username, password, businessID} = req.body;
 
         const user = await User.create({
             email, 
             username, 
-            password, 
-            businessName, 
+            password,
             businessID,
         });
 
         res.status(201).json({ message: "User"})
     } catch (error) {
 
-        console.error("Error registering employee: ", error.message);
-        res.status(500).json({ error: "Failed to register user!"});
+        console.error("Error signing in: ", error.message);
+        res.status(500).json({ error: "Failed to log in user!"});
 
     }
 };
 
-module.exports = {registerEmployee};
+module.exports = {loginEmployee};
