abffdd35e869880b881836d7e52a9e4a17cef0b9
Tristan-N09 <nadartristan@gmail.com>
2025-07-29 10:00:55 +0200

    HomePageFirstPush

---
+++ Leaf-Line/frontend/Website/HomePage/LLHomePage.js
@@ -0,0 +1,13 @@
+
+console.log("Javascript is linked succesffully");
+
+const NavPnl = document.querySelector("nav");
+const Navbtn = document.querySelector(".NavBtn");
+
+
+
+Navbtn.addEventListener('click', () => {
+    NavPnl.classList.toggle('active');//Toggles the nav class between active and not active
+})
+
+
