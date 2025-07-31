dca7fbf7af5052a74664f231f10f2db9675bb521
RohanLeePatel <rohanpatel0905@gmail.com>
2025-07-31 17:22:14 +0200

    Firebase-Stuff
    Added firebase token verification, added it into the routes too, added firebase key aswell, installed firebase-admin aswell

--- Leaf-Line/backend/routes/investorRoutes.js
+++ Leaf-Line/backend/routes/investorRoutes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { loginInvestor, registerInvestor } = require('../controllers/investorControllers');
 
+// Import Firebase token from firebase.js
+const verifyFirebaseToken = require('../firebase');
+
 router.post('/login', loginInvestor);
 router.post('/registration', registerInvestor);
+
+// Firebase token verification for investor dashboard
+router.get('/#Path Still Needs To Go Here - Probably Investor Dashboard', verifyFirebaseToken, (req, res) => {
+    res.json({ message: `Welcome Investor ${req.user.email}`, user: req.user });
+});
 
 module.exports = router;
