0ed849e129ede645013790aac8a2be2796f31c7f
b1t-0f-v1k <victorsamehumadu@gmail.com>
2025-08-07 10:26:57 +0200

    thrishen edits

--- Leaf-Line/backend/server.js
+++ Leaf-Line/backend/server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI)
     // start server only after DB connects successfully
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+        console.log(`Server running on http://localhost:${PORT}`);
     });
 })
 .catch( (err) => console.error("MongoDB connection error: ", err));
